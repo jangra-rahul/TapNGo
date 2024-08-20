f3af9e3c6d151ad4f4b0f3f9c3b9a1221a68e127
jangra-rahul <rahuljangra2122@gmail.com>
2024-08-20 18:14:03 +0530

    padding add

--- src/app/Dashboard/HomeDashboard/page.tsx
+++ src/app/Dashboard/HomeDashboard/page.tsx
@@ -80,11 +80,11 @@ const Home: React.FC = () => {
         <div className={`${showModal === true && "custom_all_dark"}`}></div>
       )}
       <div style={{ height: "96%" }} className="px-2">
-        <div className="pb-4 pt-3 h-100">
+        <div className="pb-4 pt-3 h-100 ps-2">
           <Row className="h-100">
-            <Col xl={7}>
+            <Col xl={7} className="pe-4">
               <Row className="h-100">
-                <Col md={7} className="  mt-md-0  rounded-2 ">
+                <Col md={7} className=" pe-lg-2 ps-lg-0 mt-md-0  rounded-2 ">
                   <div className="card pt-4 d-flex flex-column justify-content-between h-100 p-md-4 p-3 border-0">
                     <div>
                       <p className=" fs_18 red_ff fw-normal mb-0 dashboard-text-color">
@@ -145,7 +145,10 @@ const Home: React.FC = () => {
                   </div>
                 </Col>
 
-                <Col md={5} className="mt-4  mt-md-0  rounded-2 ">
+                <Col
+                  md={5}
+                  className="mt-4 ps-lg-3 px-lg-0 mt-md-0  rounded-2 "
+                >
                   <div className="card  d-flex flex-column justify-content-between h-100 p-md-4 p-3 border-0">
                     <div className="d-flex justify-content-between align-items-center pb-4">
                       <p className="fs_20 red_ff fw-semibold dashboard-text-color mb-0">
@@ -189,10 +192,135 @@ const Home: React.FC = () => {
                     </div>
                   </div>
                 </Col>
-                <Col xs={12} className="mt-4 bg-white">
+                <div className="px-3 d-lg-none d-block">
+                  <Col xs={12} className="mt-4 px-lg-0  bg-white rounded-3 ">
+                    <div
+                      style={{ height: "95%" }}
+                      className="card p-md-4 pt-3 px-3 border-0 overflow-auto"
+                    >
+                      <div className="d-flex justify-content-between align-items-center">
+                        <h5 className="fs_20 dashboard-text-color fw-semibold source_ff">
+                          Recent Usage
+                        </h5>
+                        <a
+                          className="tdn fw-semibold red_ff fs_14 text-decoration-underline"
+                          href="#"
+                        >
+                          View All
+                        </a>
+                      </div>
+                      <div
+                        style={{ height: "550px" }}
+                        className="overflow-auto mt-4"
+                      >
+                        <div className="d-flex mt-3 justify-content-between align-items-center">
+                          <div>
+                            <p className="mb-0 fs_16 red_ff dashboard-text-color fw-normal">
+                              Manual Recharge
+                            </p>
+                            <p className="mb-0 red_ff fs_14 text-color">
+                              04 January 2024
+                            </p>
+                          </div>
+                          <p className="fs_20 red_ff fw-semibold mb-0  dashboard-text-color">
+                            $1.33
+                          </p>
+                        </div>
+                        <div className="border mt-3"></div>
+                        <div className="d-flex mt-3 justify-content-between align-items-center">
+                          <div>
+                            <p className="mb-0 fs_16 dashboard-text-color red_ff fw-normal">
+                              Auto Top-Up
+                            </p>
+                            <p className="mb-0 red_ff fs_14 text-color">
+                              04 January 2024
+                            </p>
+                          </div>
+                          <p className="fs_20 red_ff fw-semibold mb-0 dashboard-text-color">
+                            $103.21
+                          </p>
+                        </div>
+                        <div className="border mt-3"></div>
+                        <div className="d-flex mt-3 justify-content-between align-items-center">
+                          <div>
+                            <p className="mb-0 dashboard-text-color fs_16 red_ff fw-normal">
+                              Manual Recharge
+                            </p>
+                            <p className="mb-0 red_ff fs_14 text-color">
+                              04 January 2024
+                            </p>
+                          </div>
+                          <p className="fs_20 red_ff fw-semibold mb-0 dashboard-text-color">
+                            $2.36
+                          </p>
+                        </div>
+                        <div className="border mt-3"></div>
+                        <div className="d-flex mt-3 justify-content-between align-items-center">
+                          <div>
+                            <p className="mb-0 dashboard-text-color fs_16 red_ff fw-normal">
+                              Manual Recharge
+                            </p>
+                            <p className="mb-0 red_ff fs_14 text-color">
+                              04 January 2024
+                            </p>
+                          </div>
+                          <p className="fs_20 red_ff fw-semibold mb-0 dashboard-text-color">
+                            $1.36
+                          </p>
+                        </div>
+                        <div className="border mt-3"></div>
+                        <div className="d-flex mt-3 justify-content-between align-items-center">
+                          <div>
+                            <p className="mb-0 dashboard-text-color fs_16 red_ff fw-normal">
+                              Auto Top-Up
+                            </p>
+                            <p className="mb-0 red_ff fs_14 text-color">
+                              04 January 2024
+                            </p>
+                          </div>
+                          <p className="fs_20 red_ff fw-semibold mb-0 dashboard-text-color">
+                            $5.45
+                          </p>
+                        </div>
+                        <div className="border mt-3"></div>
+                        <div className="d-flex mt-3 justify-content-between align-items-center">
+                          <div>
+                            <p className="mb-0 dashboard-text-color fs_16 red_ff fw-normal">
+                              Auto Top-Up
+                            </p>
+                            <p className="mb-0 red_ff fs_14 text-color">
+                              04 January 2024
+                            </p>
+                          </div>
+                          <p className="fs_20 red_ff fw-semibold mb-0 dashboard-text-color">
+                            $5.45
+                          </p>
+                        </div>
+                        <div className="border mt-3"></div>
+                        <div className="d-flex mt-3 justify-content-between align-items-center">
+                          <div>
+                            <p className="mb-0 dashboard-text-color fs_16 red_ff fw-normal">
+                              Auto Top-Up
+                            </p>
+                            <p className="mb-0 red_ff fs_14 text-color">
+                              04 January 2024
+                            </p>
+                          </div>
+                          <p className="fs_20 red_ff fw-semibold mb-0 dashboard-text-color">
+                            $5.45
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </Col>
+                </div>
+                <Col
+                  xs={12}
+                  className="mt-4 px-lg-0 d-lg-block d-none bg-white rounded-3 "
+                >
                   <div
                     style={{ height: "95%" }}
-                    className="card p-md-4 p-3 border-0 overflow-auto"
+                    className="card p-md-4 pt-3 px-3 border-0 overflow-auto"
                   >
                     <div className="d-flex justify-content-between align-items-center">
                       <h5 className="fs_20 dashboard-text-color fw-semibold source_ff">
@@ -311,7 +439,145 @@ const Home: React.FC = () => {
                 </Col>
               </Row>
             </Col>
-            <Col xl={5} className="mt-4 mt-xl-0">
+
+            <div className="px-3 pe-4 d-lg-none d-block">
+              <Col xl={5} className="mt-4 mt-xl-0 ">
+                <div className="card h-100 d-flex flex-column m-0 flex-grow-1 border-0 p-md-4 p-3">
+                  <div>
+                    <div className="d-md-flex flex-wrap justify-content-between align-items-center">
+                      <span className="">
+                        <p className="fs_20 fw-semibold red_ff  dashboard-text-color">
+                          {" "}
+                          Prepaid Recharge{" "}
+                        </p>
+                      </span>
+                      <div className="d-flex mt-3 mt-sm-0 text-color align-items-center gap-3">
+                        <p>Auto Top-up</p>
+                        <div className="border rounded-5 mb-3 m-0 ">
+                          <Form className="border-1  rounded-1  ps-1 pe-2 d-flex justify-content-center">
+                            <Form.Check
+                              type="switch"
+                              id="custom-switch"
+                              label={
+                                <span
+                                  className={toggleState ? "color_blue" : ""}
+                                >
+                                  {toggleState ? "On" : "Off"}
+                                </span>
+                              }
+                              onChange={(event) => {
+                                setToggleState(event.target.checked);
+                              }}
+                            />
+                          </Form>
+                        </div>
+                      </div>
+                    </div>
+
+                    <Row className="mt-4">
+                      <Col
+                        xs={4}
+                        className="pe-0 text-center"
+                        onClick={() => handleTabClick("Manual")}
+                      >
+                        <Nav.Link
+                          href="#"
+                          className={
+                            selectedTab === "Manual"
+                              ? " border_radius_blue color_blue"
+                              : ""
+                          }
+                        >
+                          <h3 className="red_ff fs_16">Manual</h3>
+                        </Nav.Link>
+                        {selectedTab === "Manual" ? (
+                          <div className="mb-2   border"></div>
+                        ) : (
+                          <>
+                            <div className="    border"></div>
+                          </>
+                        )}
+                      </Col>
+                      <Col
+                        xs={4}
+                        className="px-0 text-center"
+                        onClick={() => handleTabClick("Auto")}
+                      >
+                        <Nav.Link
+                          href="#"
+                          className={
+                            selectedTab === "Auto"
+                              ? " border_radius_blue color_blue"
+                              : ""
+                          }
+                        >
+                          <h3 className="red_ff fs_16">Auto</h3>
+                        </Nav.Link>
+                        {selectedTab === "Auto" ? (
+                          <div className="mb-3  border"></div>
+                        ) : (
+                          <>
+                            <div className="mt-2  border"></div>
+                          </>
+                        )}
+                      </Col>
+                      <Col
+                        xs={4}
+                        className="ps-0 text-center"
+                        onClick={() => handleTabClick("Subscription")}
+                      >
+                        <Nav.Link
+                          href="#"
+                          className={
+                            selectedTab === "Subscription"
+                              ? "active-tab border_radius_blue color_blue"
+                              : ""
+                          }
+                        >
+                          <h3 className="red_ff fs_16">Subscription</h3>
+                        </Nav.Link>
+                        {selectedTab === "Subscription" ? (
+                          <div className="mb-3   border"></div>
+                        ) : (
+                          <>
+                            <div className="mt-2   border"></div>
+                          </>
+                        )}
+                      </Col>
+                    </Row>
+                  </div>
+                  <Tab.Content className="mt-2 h-100">
+                    <Tab.Pane
+                      eventKey="Manual"
+                      className={
+                        selectedTab === "Manual" ? "show active h-100" : ""
+                      }
+                    >
+                      <Manual />
+                    </Tab.Pane>
+                    <Tab.Pane
+                      eventKey="Auto"
+                      className={
+                        selectedTab === "Auto" ? "show active h-100" : ""
+                      }
+                    >
+                      <Auto />
+                    </Tab.Pane>
+                    <Tab.Pane
+                      eventKey="Subscription"
+                      className={
+                        selectedTab === "Subscription"
+                          ? "show active h-100"
+                          : ""
+                      }
+                    >
+                      <Subscription />
+                    </Tab.Pane>
+                  </Tab.Content>
+                </div>
+              </Col>
+            </div>
+            <Col xl={5} className="mt-4 mt-xl-0 ps-0 ps-xl-3">
               <div className="card h-100 d-flex flex-column m-0 flex-grow-1 border-0 p-md-4 p-3">
                 <div>
                   <div className="d-md-flex flex-wrap justify-content-between align-items-center">
