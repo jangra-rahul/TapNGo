fa1a24dfa8b95ee1541527febcab4dad2f99efa3
jangra-rahul <rahuljangra2122@gmail.com>
2024-08-20 18:43:28 +0530

    width set

--- src/app/Dashboard/maindashboard/Admindashboard.tsx
+++ src/app/Dashboard/maindashboard/Admindashboard.tsx
@@ -59,155 +59,165 @@ const Admindashboard: React.FC = () => {
           breakpoint="lg"
           collapsedWidth="0"
           trigger={null}
-          className="sider-responsive bg-sidebar "
+          className="sider-responsive w-100 bg-sidebar "
           style={{
             position: "fixed",
             left: 0,
             top: 0,
             bottom: 0,
             height: "100vh",
-            // width: "100% ",
           }}
         >
-          <Link href="#">
-            <div>
-              <Image
-                className="px-3 p-4"
-                src={logo}
-                alt="logo"
-                width={200}
-                height={100}
-              />
-            </div>
-          </Link>
-          <Menu theme="dark" className="bg-sidebar">
-            <Menu.Item
-              key="1"
-              className={
-                pathname === "/Dashboard/HomeDashboard" ? "active-color" : ""
-              }
-            >
-              <Link href="/Dashboard/HomeDashboard">
-                <div className="d-flex gap-2  align-items-center">
-                  <Image
-                    src={home_icon}
-                    alt="home_icon"
-                    width={16}
-                    height={16}
-                  />
-                  <span className="mb-0 fs_14 red_ff">Home</span>
-                </div>
-              </Link>
-            </Menu.Item>
-            <Menu.Item
-              key="2"
-              className={pathname === "/Dashboard/Usages" ? "active-color" : ""}
-            >
-              <Link href="/Dashboard/Usages">
-                <div className="d-flex gap-2  align-items-center">
-                  <Image
-                    src={usage_icon}
-                    alt="Usage_icon"
-                    width={16}
-                    height={16}
-                  />
-                  <span className="mb-0 fs_14 red_ff">Usage</span>
-                </div>
-              </Link>
-            </Menu.Item>
-            <Menu.Item
-              key="3"
-              className={
-                pathname === "/Dashboard/Subcriptiondashboard"
-                  ? "active-color"
-                  : ""
-              }
-            >
-              <Link href="/Dashboard/Subcriptiondashboard">
-                <div className="d-flex gap-2  align-items-center">
-                  <Image
-                    src={subscription_icon}
-                    alt="Subscription_icon"
-                    width={16}
-                    height={16}
-                  />
-                  <span className="mb-0 fs_14 red_ff">Subscription</span>
-                </div>
-              </Link>
-            </Menu.Item>
-            <Menu.Item
-              key="4"
-              className={
-                pathname === "/Dashboard/FamilyMember" ? "active-color" : ""
-              }
-            >
-              <Link href="/Dashboard/FamilyMember">
-                <div className="d-flex gap-2  align-items-center">
-                  <Image
-                    src={Family_icon}
-                    alt="FamilyM_icon"
-                    width={16}
-                    height={16}
-                  />
-                  <span className="mb-0 fs_14 red_ff">Family Member</span>
-                </div>
-              </Link>
-            </Menu.Item>
-            <Menu.Item
-              key="5"
-              className={
-                pathname === "/Dashboard/MyOffers" ? "active-color" : ""
-              }
-            >
-              <Link href="/Dashboard/MyOffers">
-                <div className="d-flex gap-2  align-items-center">
-                  <Image
-                    src={Offers_icon}
-                    alt="Offer_icon"
-                    width={16}
-                    height={16}
-                  />
-                  <span className="mb-0 fs_14 red_ff">My Offers</span>
-                </div>
-              </Link>
-            </Menu.Item>
-            <Menu.Item
-              key="6"
-              className={pathname === "/Dashboard/FAQ" ? "active-color" : ""}
-            >
-              <Link href="/Dashboard/FAQ">
-                <div className="d-flex gap-2  align-items-center">
-                  <Image src={Faq_icon} alt="faq_icon" width={16} height={16} />
-                  <span className="mb-0 fs_14 red_ff">FAQ</span>
-                </div>
-              </Link>
-            </Menu.Item>
-            <Menu.Item
-              key="7"
-              className={
-                pathname === "/Dashboard/SupportPage" ? "active-color" : ""
-              }
-            >
-              <Link href="/Dashboard/SupportPage">
-                <div className="d-flex gap-2  align-items-center">
-                  <Image
-                    src={Support_icon}
-                    alt="Support_icon"
-                    width={16}
-                    height={16}
-                  />
-                  <span className="mb-0 fs_14 red_ff">Support</span>
-                </div>
-              </Link>
-            </Menu.Item>
-          </Menu>
+          <div>
+            <Link href="#">
+              <div>
+                <Image
+                  className="px-3 p-4"
+                  src={logo}
+                  alt="logo"
+                  width={200}
+                  height={100}
+                />
+              </div>
+            </Link>
+            <Menu theme="dark" className="bg-sidebar">
+              <Menu.Item
+                key="1"
+                className={
+                  pathname === "/Dashboard/HomeDashboard" ? "active-color" : ""
+                }
+              >
+                <Link href="/Dashboard/HomeDashboard">
+                  <div className="d-flex gap-2  align-items-center">
+                    <Image
+                      src={home_icon}
+                      alt="home_icon"
+                      width={16}
+                      height={16}
+                    />
+                    <span className="mb-0 fs_14 red_ff">Home</span>
+                  </div>
+                </Link>
+              </Menu.Item>
+              <Menu.Item
+                key="2"
+                className={
+                  pathname === "/Dashboard/Usages" ? "active-color" : ""
+                }
+              >
+                <Link href="/Dashboard/Usages">
+                  <div className="d-flex gap-2  align-items-center">
+                    <Image
+                      src={usage_icon}
+                      alt="Usage_icon"
+                      width={16}
+                      height={16}
+                    />
+                    <span className="mb-0 fs_14 red_ff">Usage</span>
+                  </div>
+                </Link>
+              </Menu.Item>
+              <Menu.Item
+                key="3"
+                className={
+                  pathname === "/Dashboard/Subcriptiondashboard"
+                    ? "active-color"
+                    : ""
+                }
+              >
+                <Link href="/Dashboard/Subcriptiondashboard">
+                  <div className="d-flex gap-2  align-items-center">
+                    <Image
+                      src={subscription_icon}
+                      alt="Subscription_icon"
+                      width={16}
+                      height={16}
+                    />
+                    <span className="mb-0 fs_14 red_ff">Subscription</span>
+                  </div>
+                </Link>
+              </Menu.Item>
+              <Menu.Item
+                key="4"
+                className={
+                  pathname === "/Dashboard/FamilyMember" ? "active-color" : ""
+                }
+              >
+                <Link href="/Dashboard/FamilyMember">
+                  <div className="d-flex gap-2  align-items-center">
+                    <Image
+                      src={Family_icon}
+                      alt="FamilyM_icon"
+                      width={16}
+                      height={16}
+                    />
+                    <span className="mb-0 fs_14 red_ff">Family Member</span>
+                  </div>
+                </Link>
+              </Menu.Item>
+              <Menu.Item
+                key="5"
+                className={
+                  pathname === "/Dashboard/MyOffers" ? "active-color" : ""
+                }
+              >
+                <Link href="/Dashboard/MyOffers">
+                  <div className="d-flex gap-2  align-items-center">
+                    <Image
+                      src={Offers_icon}
+                      alt="Offer_icon"
+                      width={16}
+                      height={16}
+                    />
+                    <span className="mb-0 fs_14 red_ff">My Offers</span>
+                  </div>
+                </Link>
+              </Menu.Item>
+              <Menu.Item
+                key="6"
+                className={pathname === "/Dashboard/FAQ" ? "active-color" : ""}
+              >
+                <Link href="/Dashboard/FAQ">
+                  <div className="d-flex gap-2  align-items-center">
+                    <Image
+                      src={Faq_icon}
+                      alt="faq_icon"
+                      width={16}
+                      height={16}
+                    />
+                    <span className="mb-0 fs_14 red_ff">FAQ</span>
+                  </div>
+                </Link>
+              </Menu.Item>
+              <Menu.Item
+                key="7"
+                className={
+                  pathname === "/Dashboard/SupportPage" ? "active-color" : ""
+                }
+              >
+                <Link href="/Dashboard/SupportPage">
+                  <div className="d-flex gap-2  align-items-center">
+                    <Image
+                      src={Support_icon}
+                      alt="Support_icon"
+                      width={16}
+                      height={16}
+                    />
+                    <span className="mb-0 fs_14 red_ff">Support</span>
+                  </div>
+                </Link>
+              </Menu.Item>
+            </Menu>
 
-          <Footer className="footer fs_14 ">
-            <p className="m-0 p-0 fs_14 red_ff font-light bg-p ps-2">
-              © 2024. Adopt Cashless.
-            </p>
-            <p className="bg-p fs_14 red_ff font-light">All rights reserved.</p>
-          </Footer>
+            <Footer className="footer fs_14 ">
+              <p className="m-0 p-0 fs_14 red_ff font-light bg-p ps-2">
+                © 2024. Adopt Cashless.
+              </p>
+              <p className="bg-p fs_14 red_ff font-light">
+                All rights reserved.
+              </p>
+            </Footer>
+          </div>
         </Sider>
       </div>
     </Layout>
